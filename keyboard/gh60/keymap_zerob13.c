926acd89d7fdbe021bdc15a060086bcb12307f04
zerob13 <i@zerob13.in>
2017-04-03 23:25:10 +0800

    add macro

--- keyboard/gh60/keymap_zerob13.c
+++ keyboard/gh60/keymap_zerob13.c
@@ -1,5 +1,7 @@
 #include "keymap_common.h"
-
+enum macro_id {
+    MAIL,
+};
 /*
  * HHKB Layout
  */
@@ -22,7 +24,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,      \
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,FN0,       \
-        NO,  LGUI,LALT,          SPC,                     NO,  RALT,RGUI,NO),
+        NO,  LGUI,LALT,          SPC,                     RALT,RGUI,NO,NO),
     /* 1: HHKB Fn layer
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
@@ -38,10 +40,10 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     KEYMAP_HHKB(
         PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,  \
-        CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS,UP,  TRNS,TRNS,      \
-        TRNS,VOLD,VOLU,MUTE,EJCT,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,     PENT,      \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,      \
-        TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+        CAPS,TRNS,TRNS,TRNS,MSEL,MPRV,MPLY,MNXT,PSCR,SLCK,PAUS,UP,  TRNS,TRNS,      \
+        RCTL,VOLD,VOLU,MUTE,EJCT,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,     PENT,      \
+        TRNS,TRNS,FN4,FN1,FN2,FN3,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,      \
+        TRNS,TRNS,TRNS,          FN5,                    TRNS,TRNS,TRNS,TRNS),
 };
 
 /*
@@ -49,4 +51,23 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_BACKLIGHT_TOGGLE(),
+    [2] = ACTION_BACKLIGHT_DECREASE(),
+    [3] = ACTION_BACKLIGHT_INCREASE(),
+    [4] = ACTION_BACKLIGHT_STEP(),
+    [5] = ACTION_MACRO(MAIL),
 };
+
+/*
+ * Macro definition
+ */
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    switch (id) {
+        case MAIL:
+            return (record->event.pressed ?
+                    MACRO( I(0), T(i), T(@), T(z), T(e), T(r), T(o), T(b), T(1), T(3), T(.), T(i), W(255), T(n), END ) :
+                    MACRO_NONE );
+    }
+    return MACRO_NONE;
+}
