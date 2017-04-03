bd58f80b9a200ee924b6773eb6c2119c71311c66
zerob13 <i@zerob13.in>
2017-04-04 00:09:20 +0800

    make 6x space hhkb layout work

--- keyboard/gh60/keymap_zerob13.c
+++ keyboard/gh60/keymap_zerob13.c
@@ -17,11 +17,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |Gui |Alt |      Space             |    |Alt |Gui |    |
      * `-----------------------------------------------------------'
      */
-    KEYMAP(
+    KEYMAP_HHKB(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,      \
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
-        LSFT,NO,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,FN0,       \
+        LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,FN0,       \
         NO,  LGUI,LALT,          SPC,                     RALT,RGUI,NO,NO),
     /* 1: HHKB Fn layer
      * ,-----------------------------------------------------------.
@@ -36,11 +36,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
-    KEYMAP(
+    KEYMAP_HHKB(
         PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,  \
         CAPS,TRNS,TRNS,TRNS,MSEL,MPRV,MPLY,MNXT,PSCR,SLCK,PAUS,UP,  TRNS,TRNS,      \
         RCTL,VOLD,VOLU,MUTE,EJCT,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,     PENT,      \
-        TRNS,TRNS,TRNS,FN4,FN1,FN2,FN3,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,      \
+        TRNS,TRNS,FN4,FN1,FN2,FN3,PPLS,PMNS,END, PGDN,DOWN,     TRNS,TRNS,      \
         TRNS,TRNS,TRNS,          FN5,                    TRNS,TRNS,TRNS,TRNS),
 };
 
@@ -67,7 +67,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     switch (id) {
         case MAIL:
             return (record->event.pressed ?
-                    MACRO( I(0), T(I),D(LSFT), T(2), U(LSFT), T(Z), T(E), T(R), T(O), T(B), T(1), T(3), T(DOT), T(I), W(255), T(N), END ) :
+                    MACRO( I(0), T(I),D(LSFT), T(2), U(LSFT), T(Z), T(E), T(R), T(O), T(B), T(1), T(3), T(DOT), T(I), T(N), END ) :
                     MACRO_NONE );
     }
     return MACRO_NONE;
