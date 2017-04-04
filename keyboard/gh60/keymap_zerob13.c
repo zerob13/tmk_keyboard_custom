549fc37c7bf7e6ef5f9436ffa734abd89154d406
zerob13 <i@zerob13.in>
2017-04-04 16:48:10 +0800

    modify keymap

--- keyboard/gh60/keymap_zerob13.c
+++ keyboard/gh60/keymap_zerob13.c
@@ -14,7 +14,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn |
      * |-----------------------------------------------------------|
-     * |    |Gui |Alt |      Space             |    |Alt |Gui |    |
+     * |    |Alt |Gui |      Space             |    |Gui |Alt |    |
      * `-----------------------------------------------------------'
      */
     KEYMAP_HHKB(
@@ -22,7 +22,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,      \
         LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,       \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,FN0,       \
-        NO,  LGUI,LALT,          SPC,                     RALT,RGUI,NO,NO),
+        NO,  LALT,LGUI,          SPC,                     RGUI,RALT,NO,NO),
     /* 1: HHKB Fn layer
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
